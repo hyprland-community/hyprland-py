55d961456258829264ec92e1ff53abe3c0a7433b
flick0 <flickerdroid211@gmail.com>
2022-12-21 17:08:29 +0530

    cleanup

--- sockets/commands.py
+++
@@ -1,9 +0,0 @@
-import socket
-import os
-
-hypr_sock = f"/tmp/hypr/{os.getenv('HYPRLAND_INSTANCE_SIGNATURE')}/.socket.sock"
-
-with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
-    s.connect(hypr_sock)
-    s.sendall(b"keyword general:col.active_border rgba(ffffffff)")
-    print(s.recv(1024).decode("utf-8"))
