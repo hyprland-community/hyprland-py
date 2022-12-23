7ac4bc240e39a124cbcb1d143c5185a33e666dfd
flick0 <flickerdroid211@gmail.com>
2022-12-23 23:22:11 +0530

    impl events

--- example_config.py
+++ example_config.py
@@ -1,6 +1,17 @@
 import hyprland
+import random
 
-c = hyprland.Config()
 
-c.decoration.rounding = 12
-c.general.border_size = 10
+class e(hyprland.Events):
+
+    def on_connect(self):
+        self.c = hyprland.Config()
+
+        self.c.decoration.rounding = 12
+        self.c.general.border_size = 10
+    
+    def on_any(self, event, *args):
+        self.c.general.border_size = random.randint(0, 20)
+
+
+e().connect()
