9466b0db9da7a7cf9240dcdf5ea5a9e11bf65b84
flick0 <flickerdroid211@gmail.com>
2022-12-27 19:05:50 +0530

    dispatchers

--- example_config.py
+++ example_config.py
@@ -6,21 +6,12 @@ class Config(hyprland.Events):
         super().__init__()
 
     async def callback(self):
-        await hyprland.Dispatch.exec("kitty") # open kitty term
+        await hyprland.Dispatch.exec("kitty --") # open kitty term
     
     async def on_connect(self):
         print("Connected to hyprland")
 
-        self.c.general.border_size = 10 # sync config
-        await self.c.decoration.set_rounding(10) # async config
-
-        await self.c.add_bind( # async bind
-            hyprland.Bind(
-                ["SUPER", "b"],
-                self.callback, # function to run when bind is triggered
-                hyprland.BindFlag.press
-            )
-        )
+        
     
 c = Config()
 
