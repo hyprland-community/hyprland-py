35d8b04067624fd5688ba4908036c7569bec1538
aspizu <aspizu@protonmail.com>
2023-09-28 02:25:18 +0530

    Start implementation of .socket2.sock support.

--- examples/flicko.py
+++ examples/flicko.py
@@ -1,1 +1,20 @@
 from __future__ import annotations
+
+from hyprland.binds import Key, Mod
+from hyprland.dispatch import (
+   Exec,
+   Exit,
+   KillActive,
+   MoveToWorkspace,
+   Workspace,
+   WorkspaceID,
+)
+from hyprland.socket import execute_batch
+
+execute_batch(
+   (Mod.SUPER + Key("M")).bind(Exit()),
+   (Mod.SUPER + Key("Return")).bind(Exec("wezterm")),
+   (Mod.SUPER + Key("Q")).bind(KillActive()),
+   *((Mod.SUPER + Key(str(i))).bind(Workspace(WorkspaceID(i))) for i in range(1, 11)),
+   *((Mod.SUPER + Mod.SHIFT + Key(str(i))).bind(MoveToWorkspace(WorkspaceID(i))) for i in range(1, 11)),
+)
